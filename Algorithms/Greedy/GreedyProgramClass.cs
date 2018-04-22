4a98bdfde7ee7fdab34901c7dd94d826742fd89f
Rushang Chauhan <chauhanrushang@gmail.com>
2018-04-22 23:49:55 +0530

    Solved: Minimum Absolute Difference in an Array, Marc's Cakewalk, Priyanka and Toys, Mark and Toys

--- Algorithms/Greedy/GreedyProgramClass.cs
+++ Algorithms/Greedy/GreedyProgramClass.cs
@@ -8,14 +8,41 @@ namespace Algorithms.Greedy
 {
     public static class GreedyProgramClass
     {
+
+        /// <summary>
+        /// Minimum Absolute Difference in an Array
+        /// </summary>
+        /// <Detail name="URL">https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem</Detail>
+        /// <Detail name="Comment"></Detail>
         internal static void MinimumAbsoluteDifferenceinanArray()
         {
-            throw new NotImplementedException();
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] Arr = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+            int MinDiff = int.MaxValue;
+
+            for (int i = 0; i < N - 1; i++)
+                if (Math.Abs(Arr[i + 1] - Arr[i]) < MinDiff)
+                    MinDiff = Math.Abs(Arr[i + 1] - Arr[i]);
+
+            Console.WriteLine(MinDiff);
         }
 
+
+        /// <summary>
+        /// Marc's Cakewalk
+        /// </summary>
+        /// <Detail name="URL">https://www.hackerrank.com/challenges/marcs-cakewalk/problem</Detail>
+        /// <Detail name="Comment"></Detail>
         internal static void MarcsCakewalk()
         {
-            throw new NotImplementedException();
+            Console.ReadLine();
+            int[] CaloriesArray = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(x => x).ToArray();
+            double result = 0;
+
+            for (int i = 0; i < CaloriesArray.Length; i++)
+                result += CaloriesArray[i] * Math.Pow(2, (double)i);
+
+            Console.WriteLine(result);
         }
 
         internal static void GetaninterviewwithHackerRank()
@@ -48,9 +75,29 @@ namespace Algorithms.Greedy
             throw new NotImplementedException();
         }
 
+
+        /// <summary>
+        /// Priyanka and Toys
+        /// </summary>
+        /// <Detail name="URL">https://www.hackerrank.com/challenges/priyanka-and-toys/problem</Detail>
+        /// <Detail name="Comment"></Detail>
         internal static void PriyankaandToys()
         {
-            throw new NotImplementedException();
+            Console.ReadLine();
+            int[] ToyWeights = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+            int NumberOfBoxes = 0;
+            int minWeight = Int32.MinValue;
+
+            foreach (int weight in ToyWeights)
+            {
+                if (weight > minWeight + 4)
+                {
+                    minWeight = weight;
+                    NumberOfBoxes++;
+                }
+            }
+
+            Console.WriteLine(NumberOfBoxes);
         }
 
         internal static void LargestPermutation()
@@ -58,9 +105,29 @@ namespace Algorithms.Greedy
             throw new NotImplementedException();
         }
 
+
+        /// <summary>
+        /// Mark and Toys
+        /// </summary>
+        /// <Detail name="URL">https://www.hackerrank.com/challenges/mark-and-toys/problem</Detail>
+        /// <Detail name="Comment"></Detail>
         internal static void MarkandToys()
         {
-            throw new NotImplementedException();
+            int[] N = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int Amount = N[1];
+            int[] ToyPrices = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+            int result = 0;
+
+            foreach (int i in ToyPrices)
+            {
+                Amount -= i;
+                if (Amount < 0)
+                    break;
+                result++;
+            }
+
+            Console.WriteLine(result);
+
         }
 
         internal static void GreedyFlorist()
